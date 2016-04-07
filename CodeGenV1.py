b52e22e5171febb3e612b4019d48848da3a4879b
JamStop <jimmyyue1@gmail.com>
2016-04-07 11:15:17 -0700

    test code gen

---
+++ CodeGenV1.py
@@ -0,0 +1,33 @@
+'''
+V1
+c Jimmy Yue
+credits to http://effbot.org/zone/python-code-generator.htm for basic write functions
+'''
+
+import string
+
+
+class VenomGen:
+    def __init__(self):
+        self.level = 0
+        self.code = []
+        self.tab = "\t"
+
+    # Basic write functionalities
+    def end(self):
+        return string.join(self.code, "")
+
+    def write(self, string):
+        self.code.append(self.tab * self.level + string)
+
+    def indent(self):
+        self.level = self.level + 1
+
+    def dedent(self):
+        if self.level == 0:
+            raise SyntaxError("")
+        self.level = self.level - 1
+    # End basic write functionalities
+
+if __name__ == "__main__":
+    test_gen = VenomGen()
