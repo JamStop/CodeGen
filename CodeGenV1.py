72526be1e8148d6e631ea9a945f992bb5ae30be2
JamStop <jimmyyue1@gmail.com>
2016-04-13 15:04:32 -0700

    header + handler generation

--- CodeGenV1.py
+++ CodeGenV1.py
@@ -10,6 +10,7 @@ class VenomGen:
         self.level = 0
         self.header = []
         self.code = []
+        self.handlers = []
         self.tab = "    "
         self.file = None
 
@@ -28,10 +29,21 @@ class VenomGen:
             raise SyntaxError("Cannot dedent below level 0")
         self.level = self.level - 1
 
-    def save(self, file_name):
+    def generate(self, file_name):
         with open(file_name + ".py", "w") as file:
+            # Header Generation
             for line in self.header:
                 file.write(line)
+            file.write("\n")
+
+            # Handler Generation
+            for handler in self.handlers:
+                for line in handler[2]:
+                    file.write(line)
+                file.write("\n")
+            file.write("\n")
+
+            # Code Generation (Routes, Models, Etc.)
             for line in self.code:
                 file.write(line)
 
@@ -46,7 +58,39 @@ class VenomGen:
 
     # Custom Syntax functionalities
     def write_route(self, route_obj):
-        return
+        route_name = route_obj["path"]
+        method = route_obj["method"]
+        keys = route_obj.keys()
+        handler = self.get_handler(route_obj)
+
+        route = "app.{}({}, {})".format(method, route_name, handler)
+
+        self.code.append(route)
+
+    # TODO: Finish up handler creation
+    def get_handler(self, route_obj):
+        route_name = route_obj["path"]
+        method = route_obj["method"]
+        handler = []
+        handler_index = None
+
+        for item in self.handlers:
+            if item[0] == route_name:
+                handler = item[2]
+                handler_index = self.handlers.index(item)
+                break
+        if handler != []:
+            pass
+        else:
+            handler_name = "Handler{}".format(len(self.handlers))
+
+            handler.append("class {}(venom.RequestHandler):\n".format(handler_name))
+            self.indent()
+            handler.append(self.block() + "pass")
+            self.dedent()
+
+            self.handlers.append((route_name, handler_name, handler))
+            return handler_name
 
     def write_header(self, header):
         self.header.append("import venom\n\n")
@@ -55,14 +99,38 @@ class VenomGen:
             self.header.append(
                 "{} = venom.Application(version=1, debug=True, protocol=venom.Protocols.JSONProtocol)\n".format(app)
             )
-        self.header.append("\n")
 
     def parse_params(self, py_object):
         self.code.append(self.tab)
 
 if __name__ == "__main__":
     test_header = {"apps": ["test_app"]}
-    test_route = {"route_type": "GET", "route_name": "/serve/"}
+    test_route = {
+        "path": "/users/:id",
+        "method": "GET",
+        "headers": [
+            {
+                "name": "X-Authorization",
+                "type": "integer"
+            }
+        ],
+        "urlParameters": [
+            {
+                "name": "id",
+                "type": "integer",
+                "required": True
+            }
+        ],
+        "queryParameters": [
+            {
+                "name": "expand",
+                "type": "array",
+                "items": {
+                    "type": "string"
+                }
+            }
+        ]
+    }
 
     v = VenomGen()
     v.start("test")
@@ -70,5 +138,5 @@ if __name__ == "__main__":
     v.write_header(test_header)
     v.write_route(test_route)
 
-    v.save("test")
+    v.generate("test")
     print(v.end())
--- test.py
+++ test.py
@@ -2,3 +2,7 @@ import venom
 
 test_app = venom.Application(version=1, debug=True, protocol=venom.Protocols.JSONProtocol)
 
+class Handler0(venom.RequestHandler):
+    pass
+
+app.GET(/users/:id, Handler0)
