06a10050846e915460b045b237578f6eb32fbfc0
JamStop <jimmyyue1@gmail.com>
2016-04-21 13:56:27 -0700

    venomgeneration.py command line

--- VenomGeneration.py
+++ VenomGeneration.py
@@ -1,18 +1,11 @@
-import CodeGenV1
+from CodeGen import VenomGen
 from sys import argv
-import json
 
 '''
-Command line is "python3 VenomGeneration {file_path} {route_obj}"
+Command line is "python3 VenomGeneration '{META}'"
 '''
 if __name__ == "__main__":
-    test_application = {"apps": ["test_app"]}
-    file_path = argv[0]
-    parsed_route = json.loads(argv[1])
+    metaobject = argv[1]
 
-    vgen = CodeGenV1.VenomGen()
-    vgen.start(file_path)
-    vgen.write_header(test_application)
-    vgen.write_route(parsed_route)
-
-    vgen.generate(file_path)
+    vgen = VenomGen.VenomGen()
+    vgen.generate(metaobject)
--- test.py
+++ test.py
@@ -18,18 +18,18 @@ test_app.GET('/users/:id1', UserHandler)
 , 'UI.2345')
 
 venom.ui(
-test_app.GET('/users/:id', UserHandler).query({
-    'file': venom.Parameters.Integer(min=1),
-    'email': venom.Parameters.String(min=5)
+test_app.GET('/users/:id', UserHandler).url({
+    'agetype': venom.Parameters.String(choices=['adult', 'child'])
 }).body({
-    'file': venom.Parameters.Integer(min=0),
     'email': venom.Parameters.String(min=2, pattern='.*'),
+    'file': venom.Parameters.Integer(min=0),
     'nested': venom.Parameters.Dict({
         'foo': venom.Parameters.Float()
     }),
-    'filename': venom.Parameters.String(min=3, characters='abcdefghijklmnop', max=100)
-}).url({
-    'agetype': venom.Parameters.String(choices=['adult', 'child'])
+    'filename': venom.Parameters.String(characters='abcdefghijklmnop', min=3, max=100)
+}).query({
+    'email': venom.Parameters.String(min=5),
+    'file': venom.Parameters.Integer(min=1)
 }).headers({
-    'X-Authorization': venom.Parameters.Integer(min=8, required=False)
+    'X-Authorization': venom.Parameters.Integer(required=False, min=8)
 }), 'UI.1234')
