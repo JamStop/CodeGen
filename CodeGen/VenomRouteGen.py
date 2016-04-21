4e140fa0ad8a8f9bb5d63ab0f492b113d4ef7c37
JamStop <jimmyyue1@gmail.com>
2016-04-21 14:29:32 -0700

    basic import boilerplate (for venom)

--- CodeGen/VenomRouteGen.py
+++ CodeGen/VenomRouteGen.py
@@ -36,6 +36,9 @@ class VenomRouteGen(CodeGenerator.Generator):
 
     def update_file(self):
         # Make changes
+        if "import venom\n" not in self.imports:
+            # TODO: Better import handling
+            self.file.insert(0, "import venom\n")
         for route in self.changed_routes:
             index = route.index
             content = route.content
--- test.py
+++ test.py
@@ -18,18 +18,18 @@ test_app.GET('/users/:id1', UserHandler)
 , 'UI.2345')
 
 venom.ui(
-test_app.GET('/users/:id', UserHandler).url({
+test_app.GET('/users/:id', UserHandler).headers({
+    'X-Authorization': venom.Parameters.Integer(min=8, required=False)
+}).url({
     'agetype': venom.Parameters.String(choices=['adult', 'child'])
 }).body({
-    'email': venom.Parameters.String(min=2, pattern='.*'),
+    'filename': venom.Parameters.String(min=3, characters='abcdefghijklmnop', max=100),
     'file': venom.Parameters.Integer(min=0),
+    'email': venom.Parameters.String(min=2, pattern='.*'),
     'nested': venom.Parameters.Dict({
         'foo': venom.Parameters.Float()
-    }),
-    'filename': venom.Parameters.String(characters='abcdefghijklmnop', min=3, max=100)
+    })
 }).query({
-    'email': venom.Parameters.String(min=5),
-    'file': venom.Parameters.Integer(min=1)
-}).headers({
-    'X-Authorization': venom.Parameters.Integer(required=False, min=8)
+    'file': venom.Parameters.Integer(min=1),
+    'email': venom.Parameters.String(min=5)
 }), 'UI.1234')
