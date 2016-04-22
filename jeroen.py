239644f56636cedf5e35a1635d19a42f6a77aa27
JamStop <jimmyyue1@gmail.com>
2016-04-21 17:50:22 -0700

    added failsafe for route loading syntax

--- jeroen.py
+++ jeroen.py
@@ -30,9 +30,9 @@ version1.POST('/login', LoginHandler).body({
 
 venom.ui(
 test_app.POST('/api/v1/signup', UserHandler).body({
-    'username': venom.Parameters.String(),
-    'ag': venom.Parameters.Integer(min=0, max=200, required=False),
-    'password': venom.Parameters.String()
+    'password': venom.Parameters.String(),
+    'ag': venom.Parameters.Integer(required=False, min=0, max=200),
+    'username': venom.Parameters.String()
 }), 'ui-3456789')
 
 
