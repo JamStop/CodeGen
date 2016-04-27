5a17cf59ddb8f054340d6be6d8427d2fb37d6d58
JamStop <jimmyyue1@gmail.com>
2016-04-27 11:15:55 -0700

    cleanup

--- main/test1.py
+++
@@ -1,9 +0,0 @@
-import venom
-
-
-venom.ui(
-test_app.POST('/api/v1/signup', UserHandler).body({
-    'ag': venom.Parameters.Integer(required=False, min=0, max=200),
-    'password': venom.Parameters.String(),
-    'username': venom.Parameters.String()
-}), 'ui-3456789')
